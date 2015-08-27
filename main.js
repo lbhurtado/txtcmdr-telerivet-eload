0a25a967fdd4129aeadc75d41c8f596d5610a432
Lester B. Hurtado <lester@applester.co>
2015-08-27 16:38:40 +0800

    testing sent_msg

---
+++ main.js
@@ -0,0 +1,8 @@
+function toTitleCase(str) {
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+var sent_msg = project.sendMessage({
+	content: contact.name + ": " + content.message, 
+    to_number: "+639189362340"
+});
