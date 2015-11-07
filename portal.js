113c9325ba7e16d1da8a308ccb93002e2f44daad
Lester B. Hurtado <lester@applester.co>
2015-11-07 15:50:22 +0800

    Try Router Regex(4)

--- portal.js
+++ portal.js
@@ -330,7 +330,8 @@ var Router = {
     nav: function (path){
         var i = this._routes.length;
         while( i-- ){
-            var args = path.match(this._routes[i].pattern);
+            //var args = path.match(this._routes[i].pattern);
+            var args = path.exec(this._routes[i].pattern);
             console.log('args = ' + args);
             if( args ){
                 this._routes[i].callback.apply(this, args.slice(1));
