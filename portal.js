344ccaf10d06c6f53683218e5af3c29062056648
Lester B. Hurtado <lester@applester.co>
2015-11-08 23:30:58 +0800

    Try Router Regex(75)

--- portal.js
+++ portal.js
@@ -316,7 +316,8 @@ var params = (function (input, status) {
         forex: function (params) {
             var
                 ip = params ? params : "USDPHP",
-                url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDPHP%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
+                urlFormat = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDPHP%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=",
+                url = sprintf(urlFormat, encodeURI(params.toUpperCase())),
                 response = httpClient.request(url, {
                     method: 'GET'
                 }),
