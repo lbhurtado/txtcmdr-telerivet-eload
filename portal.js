343974aefb56fe6b41edf2c2d51bd25f14646baa
Lester B. Hurtado <lester@applester.co>
2015-11-13 10:40:38 +0800

    Add default params(11)

--- portal.js
+++ portal.js
@@ -421,7 +421,7 @@ var params = (function (input, phone_number, status, vars) {
                             .replace(/\*\w+/, '[ \t]*([^\n\r]*)') //everything after >
                             .replace(/\w+=\w+/g, '(\\w+=\\w+)\\b') //query string after ?
                         ;
-                    var re = /%(\w+)/i;
+                    var re = new RegExp("%(\\w+)");
                     regex = regex.replace(re, re.$1);
 
                     console.log('regex = ' + regex);
