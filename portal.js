5083ea3e29976c58f87c4a266475ff55e2fa3c04
Lester B. Hurtado <lester@applester.co>
2015-11-14 17:35:09 +0800

    Add news sysntax(40)

--- portal.js
+++ portal.js
@@ -32,7 +32,8 @@ var params = (function (input, phone_number, status, vars) {
             return str.replace(/&#([0-9]{1,3});/gi, function (match, numStr) {
                 var num = parseInt(numStr, 10); // read num as normal number
                 return String.fromCharCode(num);
-            });
+            })
+                .replace(/&rsquo;/g, "'");
         }
     });
 
