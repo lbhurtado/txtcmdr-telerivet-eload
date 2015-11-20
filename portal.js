a0b2620e9bec5606c980f967068d8a0f6efbb00d
Lester B. Hurtado <lester@applester.co>
2015-11-20 10:16:59 +0800

    Add addforward(53)

--- portal.js
+++ portal.js
@@ -1204,13 +1204,11 @@ var params = (function (vtelerivet) {
                     console.log('args[0] = ' + args[0]);
                     shouldAppend = (args[0].toUpperCase() === 'ADD');
                     var ar = _(args).toArray().slice(1);
-                    //return _(ar).map(function (number) {
-                    //        return number ? Library.formalize(number) : null;
-                    //});
-                    return _(_(ar)
+
+                    return _(ar)
                         .filter(function (number) {
                             return number;
-                        }))
+                        })
                         .map(function (number) {
                             //return number ? Library.formalize(number) : null;
                             return Library.formalize(number);
