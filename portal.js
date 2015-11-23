06dd73e2af078abf852ea34bd66a953c469a8c8c
Lester B. Hurtado <lester@applester.co>
2015-11-23 14:24:01 +0800

    Add get autoreply(20)

--- portal.js
+++ portal.js
@@ -571,7 +571,7 @@ var params = (function (vtelerivet) {
 
     var Router = {
         routes: {
-            'join $group *username': "join",
+            'join --group *username': "join",
             'baligod *username': "baligod",
             'passage*params': "passage",
             'info': "info",
@@ -602,7 +602,7 @@ var params = (function (vtelerivet) {
             'ping': "syntax",
             'define *word': "define",
 
-            'broadcast $group *message': "broadcast",
+            'broadcast --group *message': "broadcast",
             '@$group *message': "broadcast",
 
             'default (location|news) *params': "default",
@@ -619,8 +619,8 @@ var params = (function (vtelerivet) {
             'auto[-_\\s]?forward': "auto_forward",
             'auto[-_\\s]?forward (remove|cut|delete)': "auto_forward_remove",
             '(get|check|set|replace|add|append|insert|delete|cut|remove|clear|empty|unset) forwards?\\s?(0\\d{3}\\d{7}|63\\d{3}\\d{7}|\\+63\\d{3}\\d{7})*\\D*(0\\d{3}\\d{7}|63\\d{3}\\d{7}|\\+63\\d{3}\\d{7})*\\D*(0\\d{3}\\d{7}|63\\d{3}\\d{7}|\\+63\\d{3}\\d{7})*\\D*': "forwards",
-            '(?:get\\s|?)$option': "get",
-            'set $key $option *value': "set",
+            '(?:get\\s|#)--option': "get",
+            'set --key --option *value': "set",
             'ring': "ring"
 
         },
@@ -632,7 +632,7 @@ var params = (function (vtelerivet) {
                     var
                         regex = route
                             //.replace(/:\w+/g, '(\\w+)')
-                            .replace(/\?\w+/g, '(\\w+)')
+                            .replace(/--\w+/g, '(\\w+)')
                             //.replace(/\(([\/]?[^\)]+)\)/g, "($1)")
                             .replace(/<([\/]?[^\)]+)>/g, "($1)")
                             .replace(/%(\w+)/g, "($1)") //default value
