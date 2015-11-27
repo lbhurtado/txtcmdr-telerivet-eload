839980c469583fbf2471a25d02754f2b57b8eb80
Lester B. Hurtado <lester@applester.co>
2015-11-27 18:33:12 +0800

    Add getsetting(3)

--- portal.js
+++ portal.js
@@ -636,7 +636,7 @@ var params = (function (vtelerivet) {
             'ring': "ring",
             //'[append|replace] [autoreply|forwards] [list|array|json] *value \"(.*?)\"': "ultimateset"
             '[append|replace|delete] [string|array|list|querystring|json] [autoreply|forwards] *value': "setsetting",
-            'get $setting': "getsetting"
+            'get $format $key': "getsetting"
         },
         init: function () {
             this._routes = [];
