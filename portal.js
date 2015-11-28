adb64cfa75c26a4b276fa4ec9ef4459d13b7ed5b
Lester B. Hurtado <lester@applester.co>
2015-11-29 07:06:18 +0800

    Debug towns url(5)

--- portal.js
+++ portal.js
@@ -625,8 +625,8 @@ var params = (function (vtelerivet) {
             'regions ((?!L|V|M).)': "regions_error",
             'provinces (1|2|3|4A|4B|5|6|7|8|9|10|11|12|13|NCR|CAR|ARMM|NEGROS)': "provinces",
             'provinces ((?!1|2|3|4A|4B|5|6|7|8|9|10|11|12|13|NCR|CAR|ARMM|NEGROS).)': "provinces_error",
-            'towns ([0][1-9]{3})': "towns",
-            'towns (?![0][1-9]{3})': "towns_error",
+            'towns (0[1-9][1-9][1-9])': "towns",
+            'towns (?!0[1-9][1-9][1-9][1-9])': "towns_error",
 
             //'auto[-_\\s]?forward': "auto_forward",
             //'auto[-_\\s]?forward (remove|cut|delete)': "auto_forward_remove",
@@ -1281,7 +1281,7 @@ var params = (function (vtelerivet) {
             generatedParams.reply = reply;
             generatedParams.state = nextState;
         },
-        townsa_error: function () {
+        towns_error: function () {
             generatedParams.reply = vars.lastReply;
         },
         town: function (vtown_code) {
