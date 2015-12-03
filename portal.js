57bd829ff957796af7986a5d746bc15bfcbec698
Lester B. Hurtado <lester@applester.co>
2015-12-03 10:45:17 +0800

    Add profile to confirm instead(2)

--- portal.js
+++ portal.js
@@ -819,7 +819,7 @@ var params = (function (vtelerivet) {
                         method: 'POST'
                     }),
                 missive = {
-                    content: "Thank you.  You are now part of our campaign. You will receive news and messages from our HQ. https://twitter.com/lbhurtado/status/671920947062923264",
+                    content: "Thank you.  You are now part of our campaign. You will receive news and messages from our HQ.\shttps://twitter.com/77txtcmdr/status/672244567761096704",
                     to_number: destination
                 },
                 nextState = null;
