83d94b4d9ed693886a10095d81f93015fcccf70a
Lester Hurtado <lester@applester.co>
2016-05-05 10:17:03 +0800

    Add auto reply

--- portal.js
+++ portal.js
@@ -681,7 +681,7 @@ var params = (function (vtelerivet) {
                 content = JSON.parse(response.content);
 
             //console.log(content);
-            //generatedParams.reply = "You message was " + vmessage;
+            generatedParams.reply = "Your message is being processed. Thank you.";
         },
         join: function (vgroup, vusername) {
             var
